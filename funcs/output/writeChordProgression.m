71866a37b02fc3ab22619efec0d26f41c5f732bb
tangkk <dengjunqi0632311@gmail.com>
2015-05-15 15:52:55 +0800

    bug fix

--- funcs/output/writeChordProgression.m
+++ funcs/output/writeChordProgression.m
@@ -1,7 +1,7 @@
 % write out the chord progression as txt file according to the MIREX
 % standard: http://www.music-ir.org/mirex/wiki/2015:Audio_Chord_Estimation
 function writeChordProgression(outfolder, outpath, nslices, hopsize, fs,...
-    outchordogram, outboundaries, endtime)
+    rootgram, treblegram, bdrys, endtime, chordmode)
 
 if exist(outfolder, 'file') == 0
     mkdir(outfolder);
@@ -12,23 +12,22 @@ formatSpec2 = '%s\n';
 tw = ((hopsize/fs)*(0:nslices));
 
 % add non-chord before start if needed
-if outboundaries(1) ~= 1
-    sec0 = tw(outboundaries(1));
+if bdrys(1) ~= 1
+    sec0 = tw(bdrys(1));
     timestr0 = num2str(sec0);
     s = ['0', ' ', timestr0, ' ', 'N'];
     fprintf(fw, formatSpec2, s);
 end
 
-lenoutchordogram = length(outchordogram);
-for i = 1:1:lenoutchordogram
-    sec1 = tw(outboundaries(i));
+nchords = size(rootgram,2);
+for i = 1:1:nchords
+    sec1 = tw(bdrys(i));
     timestr1 = num2str(sec1);
-    sec2 = tw(outboundaries(i+1));
+    sec2 = tw(bdrys(i+1));
     timestr2 = num2str(sec2);
-    chordstr = outchordogram{i};
-%     if enCast2MajMin
-%         chordstr = cast2MajMin(chordstr, nfSeq(:,i)); % substitute sus to majmin
-%     end
+    root = rootgram(i);
+    treble = treblegram(i);
+    chordstr = strcat(num2note(root), ':', chordmode{2,treble});
     s = [timestr1, ' ', timestr2, ' ', chordstr];
     fprintf(fw, formatSpec2, s);
 end
