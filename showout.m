4d46fd1509b2807c0e428cb5d4a265e7271a2704
tangkk <dengjunqi0632311@gmail.com>
2015-06-30 01:25:32 +0800

    showout

---
+++ showout.m
@@ -0,0 +1,9 @@
+% show the outputs
+disp(sufix);
+dbtype(evaloutseg,'3:7');
+dbtype(evaloutbass,'3:7');
+dbtype(evaloutroot,'3:7');
+dbtype(evaloutmajmin,'3:7');
+dbtype(evaloutmajminbass,'3:7');
+dbtype(evaloutsevenths,'3:7');
+dbtype(evaloutseventhsbass,'3:7');
