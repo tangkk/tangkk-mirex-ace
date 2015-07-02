19a30acaea0d92eada78ab2d7ae1c11e0331550f
tangkk <dengjunqi0632311@gmail.com>
2015-07-02 21:31:44 +0800

    refactor

--- showout.m
+++
@@ -1,9 +0,0 @@
-% show the outputs
-disp(sufix);
-dbtype(evaloutseg,'3:7');
-dbtype(evaloutbass,'3:7');
-dbtype(evaloutroot,'3:7');
-dbtype(evaloutmajmin,'3:7');
-dbtype(evaloutmajminbass,'3:7');
-dbtype(evaloutsevenths,'3:7');
-dbtype(evaloutseventhsbass,'3:7');
