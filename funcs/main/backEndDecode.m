9c324bc0f2c638914e248fd0f7f690607c17504b
tangkk <dengjunqi0632311@gmail.com>
2015-07-07 15:04:39 +0800

    refactor
    delete unused code, reorder code
    clean up code

--- funcs/main/backEndDecode.m
+++ funcs/main/backEndDecode.m
@@ -2,16 +2,16 @@ function [rootgram, bassgram, treblegram, bdrys] = backEndDecode(chordmode, bepa
     basegram, uppergram, bdrys, nslices, df, enPlot)
 
 if beparam.useDBN1
-    bnet = dbnSetup(chordmode, dbnparam);
-    [rootgram, bassgram, treblegram] = dbnInference(bnet, chordmode, basegram, uppergram);
+bnet = dbnSetup(chordmode, dbnparam);
+[rootgram, bassgram, treblegram] = dbnInference(bnet, chordmode, basegram, uppergram);
 elseif beparam.useDBN2
-    bnet2 = dbnSetup2(chordmode, dbn2param);
-    chordogram = computeChordogram(basegram, uppergram, chordmode, dbn2param);
-    [rootgram, bassgram, treblegram] = dbnInference2(bnet2, chordmode, chordogram);
-    if df && enPlot
-    myImagePlot(chordogram, 1:size(chordogram,2), 1:size(chordogram,1),...
-    'chord progression order', 'chord', 'chordogram');
-    end
+bnet2 = dbnSetup2(chordmode, dbn2param);
+chordogram = computeChordogram(basegram, uppergram, chordmode, dbn2param);
+[rootgram, bassgram, treblegram] = dbnInference2(bnet2, chordmode, chordogram);
+if df && enPlot
+myImagePlot(chordogram, 1:size(chordogram,2), 1:size(chordogram,1),...
+'chord progression order', 'chord', 'chordogram');
+end
 end
 
 if beparam.enChordGestalt
@@ -30,8 +30,5 @@ myLinePlot(1:nchords, bassgram, 'chord progression order', 'semitone',...
     nchords, 12, 'o', 'bassgram', 0:12, bassnotenames);
 myLinePlot(1:nchords+1, bdrys, 'chord progression order', 'slice',...
     nchords, nslices, 'o', 'boundaries');
-end
-
-if df && enPlot
-    visualizeChordProgression(rootgram, bassgram, treblegram, bdrys, chordmode);
+visualizeChordProgression(rootgram, bassgram, treblegram, bdrys, chordmode);
 end
--- funcs/main/frontEndDecode.m
+++ funcs/main/frontEndDecode.m
@@ -3,7 +3,6 @@ function [bdrys, basegram, uppergram, nslices, endtime] = frontEndDecode(audiopa
 
 x = myInput(audiopath, feparam.stereotomono, feparam.fs);
 
-% note dictionary
 fmin = 27.5; % MIDI note 21, Piano key number 1 (A0)
 fmax = 3322; % MIDI note 104, Piano key number nnotes
 fratio = 2^(1/(12*3)); % nsemitones = 3
@@ -34,32 +33,19 @@ end
 % calculate note salience matrix of the stft spectrogram (cosine
 % similarity) (note that this is an additive approach, as contrast to
 % the nnls approach which is an deductive approach)
-if ~feparam.enCQT
-    % transform time domain into frequency domain
-    X = mySpectrogram(x, feparam.wl, feparam.hopsize);
-%     X = abs(spectrogram(x, hann(feparam.wl), feparam.wl - feparam.hopsize));
-    nslices = size(X,2);
-    endtime = (1/feparam.fs)*length(x);
-    if df && enPlot
-    myImagePlot(X, 1:nslices, 1:feparam.wl/2+1, 'slice', 'bin', 'spectrogram');
-    end
-    
-    if feparam.enCosSim
-    Ss = Ms*X;
-    Sc = Mc*X;
-    elseif feparam.enlogFreq
-    Ss = LE*X;
-    end
-elseif feparam.enCQT
-    % FIXME: CQT is not working as it should be
-%     [~,et] = gtTuning([0;0;0], tunpath);
-%     ptun = (log(et / 440) / log(2)) * 36; % compensate for the tunings
-    Xcq = cqt(x, 36, feparam.fs, fmin*2^((-1)/(12*3)), fmax*2^((1)/(12*3)), 'rasterize', 'full');
-    Ss = full(abs(Xcq.c));
-    Ss = [zeros(1,size(Ss,2));Ss];
-    feparam.hopsize = Xcq.xlen / size(Ss,2);
-    nslices = size(Ss,2);
-    endtime = (1/feparam.fs)*length(x);
+% transform time domain into frequency domain
+X = mySpectrogram(x, feparam.wl, feparam.hopsize);
+nslices = size(X,2);
+endtime = (1/feparam.fs)*length(x);
+if df && enPlot
+myImagePlot(X, 1:nslices, 1:feparam.wl/2+1, 'slice', 'bin', 'spectrogram');
+end
+
+if feparam.enCosSim
+Ss = Ms*X;
+Sc = Mc*X;
+elseif feparam.enlogFreq
+Ss = LE*X;
 end
 
 if df && enPlot
@@ -123,13 +109,29 @@ end
 % noise reduction process
 if feparam.enPeakNoiseRed
 nt = 0.1;
-Ssn = noteSalienceNoiseReduce(Ss, nt);
+Ss = noteSalienceNoiseReduce(Ss, nt);
 if feparam.enCosSim
-Scn = noteSalienceNoiseReduce(Sc, nt);
+Sc = noteSalienceNoiseReduce(Sc, nt);
 end
 if df && enPlot
-myImagePlot(Ssn, 1:nslices, 1:ntones, 'slice', '1/3 semitone', 'noised reduced simple tone salience matrix');
+myImagePlot(Ss, 1:nslices, 1:ntones, 'slice', '1/3 semitone', 'noised reduced simple tone salience matrix');
 % myImagePlot(Scn, 1:nslices, 1:ntones, 'slice', '1/3 semitone', 'noised reduced complex tone salience matrix');
+end
+end
+
+if feparam.enGesComp
+st = 0.0;
+Ss = compensateLongSalience(Ss,feparam.wgmax,st,0,0);
+if df && enPlot
+myImagePlot(Ss, 1:nslices, 1:nnotes, 'slice', 'semitone', 'positive gestalt note salience matrix');
+end
+end
+
+if feparam.enGesRed
+st = 0.0;
+Ss = reduceShortSalience(Ss,feparam.wgmax,st,0,0);
+if df && enPlot
+myImagePlot(Ss, 1:nslices, 1:nnotes, 'slice', 'semitone', 'negative gestalt note salience matrix');
 end
 end
 
@@ -149,81 +151,23 @@ if feparam.enCosSim
     Ss = Ss.*Sc;
 end
 
-if ~feparam.tuningBefore
-% tuning algorithm, assuming nsemitones = 3
-if feparam.globalTuning
-    Ss = globalTuning(Ss);
-elseif feparam.localTuning
-    Ss = localTuning(Ss);
-elseif feparam.phaseTuning
-    [Ss,~] = phaseTuning(Ss);
-elseif feparam.gtTuning
-    [Ss,~] = gtTuning(Ss, tunpath);
-elseif feparam.vampTuning
-    [Ss,~] = vampTuning(Ss,vamptunpath);
-end
-if df && enPlot
-myImagePlot(Ss, 1:nslices, 1:ntones, 'slice', '1/3 semitone', 'tuned simple tone salience matrix');
-end
-end
-
 % nnls approximate note transcription - 3-semitones -> 1-semitone
 if feparam.enNNLS
-Sapx = nnlsTrans(Ss,E,nnotes,feparam.enSigifBins);
+S = nnlsTrans(Ss,E,nnotes,feparam.enSigifBins);
+elseif feparam.en3bin
+S = Ss(1:3:end,:) + Ss(2:3:end,:) + Ss(3:3:end,:);
+elseif feparam.enCenterbin
+S = Ss(2:3:end,:);
+end
 if df && enPlot
-myImagePlot(Sapx, 1:nslices, 1:nnotes, 'slice', 'semitone', 'nnls salience matrix');
-end
-end
-
-% interface with output S
-if feparam.enCosSim % the baseline approach
-    if feparam.enCenterbin
-        S = SssCen;
-    else 
-        S = Sss3bin;
-    end
-elseif feparam.enNNLS % nnls
-        S = Sapx;
-end
-
-if df && enPlot
-myImagePlot(S, 1:nslices, 1:nnotes, 'slice', 'semitone', 'note salience matrix');
-end
-
-
-% interface the original salience to a gestaltized salience
-Sg = S;
-
-if feparam.enGesComp
-st = 0.0;
-Sg = compensateLongSalience(Sg,feparam.wgmax,st,0,0);
-if df && enPlot
-myImagePlot(Sg, 1:nslices, 1:nnotes, 'slice', 'semitone', 'positive gestalt note salience matrix');
-end
-end
-
-if feparam.enGesRed
-st = 0.0;
-Sg = reduceShortSalience(Sg,feparam.wgmax,st,0,0);
-if df && enPlot
-myImagePlot(Sg, 1:nslices, 1:nnotes, 'slice', 'semitone', 'negative gestalt note salience matrix');
-end
-end
-
-if feparam.useBassOnsetSegment || feparam.useBassOnsetMedianSegment
-Shc = harmonicSegmentation(Sg, feparam.wgmax, 0, enPlot);
-end
-
-if feparam.useOriginalSalience
-    SpreSeg = S;
-elseif feparam.useGestaltSalience
-    SpreSeg = Sg;
+myImagePlot(S, 1:nslices, 1:nnotes, 'slice', 'semitone', 'nnls salience matrix');
 end
 
 % use different segmentation methods
 if feparam.noSegmentation
     bdrys = 1:nslices;
 elseif feparam.useBassOnsetSegment
+    Shc = harmonicSegmentation(Ss, feparam.wgmax, 0, enPlot);
     bdrys = Shc;
 elseif feparam.useWgSegment
     bdrys = 1:feparam.wgmax:nslices;
@@ -235,6 +179,7 @@ elseif feparam.useBeatSyncSegment
     btrack(btrack == 0) = 1;
     bdrys = btrack;
 elseif feparam.useBassOnsetMedianSegment
+    Shc = harmonicSegmentation(Ss, feparam.wgmax, 0, enPlot);
     bdrys = 1:round(median(Shc(2:end) - Shc(1:end-1))):nslices;
 end
 
@@ -246,14 +191,14 @@ if bdrys(1) ~= 1
 end
 
 if feparam.noSegmentation
-    Sseg = SpreSeg;
+    Sseg = S;
 else
-    Sseg = zeros(size(SpreSeg,1), length(bdrys) - 1);
+    Sseg = zeros(size(S,1), length(bdrys) - 1);
     for j = 1:size(Sseg,2)
         if feparam.useMedianFilter
-            Sseg(:,j) = median(SpreSeg(:,bdrys(j):bdrys(j+1)),2);
+            Sseg(:,j) = median(S(:,bdrys(j):bdrys(j+1)),2);
         elseif feparam.useMeanFilter
-            Sseg(:,j) = mean(SpreSeg(:,bdrys(j):bdrys(j+1)),2);
+            Sseg(:,j) = mean(S(:,bdrys(j):bdrys(j+1)),2);
         end
     end
 end
@@ -269,24 +214,24 @@ ht = hann(nnotes); ht = ht ./ norm(ht,1);
 hb = [hann(nnotes/2);zeros(nnotes/2,1)]; hb = hb ./ norm(hb,1);
 mht = repmat(ht,1,nsegs);
 mhb = repmat(hb,1,nsegs);
-Stsegout = Sseg .* mht;
-Sbsegout = Sseg .* mhb;
+Stout = Sseg .* mht;
+Sbout = Sseg .* mhb;
 if df && enPlot
-myImagePlot(Stsegout, 1:nsegs, 1:nnotes, 'slice', 'semitone', 'treble note salience matrix');
+myImagePlot(Stout, 1:nsegs, 1:nnotes, 'slice', 'semitone', 'treble note salience matrix');
 end
 if df && enPlot
-myImagePlot(Sbsegout, 1:nsegs, 1:nnotes, 'slice', 'semitone', 'bass note salience matrix');
+myImagePlot(Sbout, 1:nsegs, 1:nnotes, 'slice', 'semitone', 'bass note salience matrix');
 end
 end
 
 % compute basegram and uppergram (based on harmonic change matrix)
 if feparam.enProfiling && feparam.btchromagram
     % note this is to compute chromagram
-    basegram = computeChromagram(Sbsegout);
-    uppergram = computeChromagram(Stsegout);
+    basegram = computeChromagram(Sbout);
+    uppergram = computeChromagram(Stout);
 elseif feparam.enProfiling
-    basegram = computeBasegram(Sbsegout);
-    uppergram = computeUppergram(Stsegout);
+    basegram = computeBasegram(Sbout);
+    uppergram = computeUppergram(Stout);
 else
     basegram = computeBasegram(Sseg);
     uppergram = computeUppergram(Sseg);
