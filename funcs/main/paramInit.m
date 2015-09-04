7c295eca47924388535c02d7628e3486fac9b093
tangkk <dengjunqi0632311@gmail.com>
2015-09-04 13:07:15 +0800

    param update

--- funcs/main/paramInit.m
+++ funcs/main/paramInit.m
@@ -10,4 +10,14 @@ function [feparam, beparam, dbnparam, dbn2param, chordmode] = paramInit()
 
 % [feparam, beparam, dbnparam, dbn2param, chordmode] = paramInit5();
 
-[feparam, beparam, dbnparam, dbn2param, chordmode] = paramInit6();
+% [feparam, beparam, dbnparam, dbn2param, chordmode] = paramInit6();
+
+% [feparam, beparam, dbnparam, dbn2param, chordmode] = paramInit7();
+
+% [feparam, beparam, dbnparam, dbn2param, chordmode] = paramInit8();
+
+% [feparam, beparam, dbnparam, dbn2param, chordmode] = paramInit9();
+
+% [feparam, beparam, dbnparam, dbn2param, chordmode] = paramInit10();
+
+[feparam, beparam, dbnparam, dbn2param, chordmode] = paramInit11();
