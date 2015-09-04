bd391ccc5a80fc2e5f7b9dd948f8dcec9d90e41c
tangkk <dengjunqi0632311@gmail.com>
2015-09-04 13:06:49 +0800

    bass treble correct update

--- funcs/backend/bassTrebleCorrect.m
+++ funcs/backend/bassTrebleCorrect.m
@@ -21,35 +21,100 @@ for j = 1:nslices
     % hsr = bd1:bd2; % harmonic stable region
     hsrbdb = bd1:min(bd1 + floor((bd2 - bd1 + 1)*bdb), bd2);
     bgbdb = basegram(:,hsrbdb);
-    ugbdb = uppergram(:, hsrbdb);
+    ugbdb = uppergram(:,hsrbdb);
     sumbg = sum(bgbdb,2);
     sumug = sum(ugbdb,2);
     % cal new bass
     [~,bass] = max(sumbg);
-    % bass correct: only correct those wrong chord inversions cases
+    % inversion correct: only correct those wrong chord inversions cases
     % case1: the recognized result is an inversion but actually it is not
-        % subcase1 -> cr != cb && cr == nb, such as (Emaj/5 <- Emaj, Emaj/3 <- Emaj), use
+        % subcase1 -> cr != cb && cr == nb, such as (Emaj/5 -> Emaj, Emaj/3 -> Emaj), use
         % basegram to judge
-        % subcase2 -> cr != cb && cb == nb, such as (Emaj/5 <- Bmaj, Emaj/3 <- G#min), use
+        % subcase2 -> cr != cb && cb == nb, such as (Emaj/5 -> Bmaj, Emaj/3 -> G#min), use
         % uppergram to judge
     % case2: the recognized result is not an inversion but actually it is
-        % subcase1 -> cr == cb && cr != nb, such as (Emaj <- Emaj/5, Emaj <- Emaj/3), use
+        % subcase1 -> cr == cb && cr != nb, such as (Emaj -> Emaj/5, Emaj -> Emaj/3), use
         % basegram to judge
-        % subcase2 -> cr == cb && cb == nb, such as (Bmaj <- Emaj/5, G#min <- Emaj/3), use
+        % subcase2 -> cr == cb && cb == nb, such as (Bmaj -> Emaj/5, G#min -> Emaj/3), use
         % uppergram to judge
     % no other case is considered for now
     % the process assumes the root is correct
+    
+    % sevenths correct
+    % case3: the recognized result is majmin but actually it's sevenths
+        % subcases: maj -> maj7 or 7, min -> min7
+    % case4: the recognized result is seventh but actually it's majmin
+        % subcases: maj7 -> maj, 7 -> maj, min7 -> min
+    % the process assumes the majmin is correct
+    
     cb = bassgram(j);
     cr = rootgram(j);
     ct = treblegram(j);
     nb = bass;
     
+    tmaj = chordmode{1, tname2tnum('maj', chordmode)};
+    tmin = chordmode{1, tname2tnum('min', chordmode)};
+    tmaj7 = chordmode{1, tname2tnum('maj7', chordmode)};
+    tmin7 = chordmode{1, tname2tnum('min7', chordmode)};
+    t7 = chordmode{1, tname2tnum('7', chordmode)};
+    
+    wmaj = chordmode{3, tname2tnum('maj', chordmode)};
+    wmin = chordmode{3, tname2tnum('min', chordmode)};
+    wmaj7 = chordmode{3, tname2tnum('maj7', chordmode)};
+    wmin7 = chordmode{3, tname2tnum('min7', chordmode)};
+    w7 = chordmode{3, tname2tnum('7', chordmode)};
+    
+    smaj = wmaj*sumug(pitchTranspose(cr,tmaj));
+    smin = wmin*sumug(pitchTranspose(cr,tmin));
+    smaj7 = wmaj7*sumug(pitchTranspose(cr,tmaj7));
+    smin7 = wmin7*sumug(pitchTranspose(cr,tmin7));
+    s7 = w7*sumug(pitchTranspose(cr,t7));
+    
+    % seventh correct -------------------------------------------------
+    if cb ~= 0 && cr == cb
+        if strcmp(chordmode{2,ct},'maj')
+            if smaj7 > smaj || s7 > smaj
+                if smaj7 > s7
+                   treblegram(j) = tname2tnum('maj7', chordmode); 
+                else
+                   treblegram(j) = tname2tnum('7', chordmode);
+                end
+            end
+        end
+        
+        if strcmp(chordmode{2,ct},'min')
+            if smin7 > smin
+                treblegram(j) = tname2tnum('min7', chordmode); 
+            end
+        end
+        
+        if strcmp(chordmode{2,ct},'maj7')
+            if smaj > smaj7
+                treblegram(j) = tname2tnum('maj', chordmode); 
+            end
+        end
+        
+        if strcmp(chordmode{2,ct},'7')
+            if smaj > s7
+                treblegram(j) = tname2tnum('maj', chordmode); 
+            end
+        end
+        
+        if strcmp(chordmode{2,ct},'min7')
+            if smin > smin7
+                treblegram(j) = tname2tnum('min', chordmode); 
+            end
+        end
+    end
+    
+    % inversion correct -----------------------------------------------
     % **************************case 1***********************************
     
     % subcase1 -> cr != cb && cr == nb, such as (Emaj/5 <- Emaj, Emaj/3 <- Emaj), use
     % basegram to judge
     if cb ~= 0 && cr ~= cb && cr == nb
-        if strcmp(chordmode{2,ct},'maj/3') 
+        % triad case - majminbass
+        if strcmp(chordmode{2,ct},'maj/3')
             bassgram(j) = cr;
             treblegram(j) = tname2tnum('maj', chordmode);
         end
@@ -57,28 +122,81 @@ for j = 1:nslices
             bassgram(j) = cr;
             treblegram(j) = tname2tnum('maj', chordmode);
         end
+        
+        if strcmp(chordmode{2,ct},'min/b3')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('min', chordmode);
+        end
+        if strcmp(chordmode{2,ct},'min/5')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('min', chordmode);
+        end
+        
+        % tetrad case - sevenths bass
+        if strcmp(chordmode{2,ct},'maj7/3')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('maj7', chordmode);
+        end
+        if strcmp(chordmode{2,ct},'maj7/5')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('maj7', chordmode);
+        end
+        if strcmp(chordmode{2,ct},'maj7/7')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('maj7', chordmode);
+        end
+        
+        if strcmp(chordmode{2,ct},'7/3')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('7', chordmode);
+        end
+        if strcmp(chordmode{2,ct},'7/5')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('7', chordmode);
+        end
+        if strcmp(chordmode{2,ct},'7/b7')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('7', chordmode);
+        end
+        
+        if strcmp(chordmode{2,ct},'min7/b3')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('min7', chordmode);
+        end
+        if strcmp(chordmode{2,ct},'min7/5')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('min7', chordmode);
+        end
+        if strcmp(chordmode{2,ct},'min7/b7')
+            bassgram(j) = cr;
+            treblegram(j) = tname2tnum('min7', chordmode);
+        end
     end
     
-    % subcase2 -> cr != cb && cb == nb, such as (Emaj/5 <- Bmaj, Emaj/3 <- G#min), use
-    % uppergram to judge
-    if cb ~= 0 && cr ~= cb && cb == nb
-        if strcmp(chordmode{2,ct},'maj/3') && cb == nb
-            s1 = sumug(pitchTranspose(cb,3)) + sumug(pitchTranspose(cb,8)); % maj/3 upper
-            s2 = sumug(pitchTranspose(cb,3)) + sumug(pitchTranspose(cb,7)); % min upper
-            if s2 > s1
-                rootgram(j) = cb;
-                treblegram(j) = tname2tnum('min', chordmode);
-            end
-        end
-        if strcmp(chordmode{2,ct},'maj/5') && cb == nb
-            s1 = sumug(pitchTranspose(cb,5)) + sumug(pitchTranspose(cb,9)); % maj/5 upper
-            s2 = sumug(pitchTranspose(cb,4)) + sumug(pitchTranspose(cb,7)); % maj upper
-            if s2 > s1
-                rootgram(j) = cb;
-                treblegram(j) = tname2tnum('maj', chordmode);
-            end
-        end
-    end
+%     % subcase2 -> cr != cb && cb == nb, such as (Emaj/5 <- Bmaj, Emaj/3 <- G#min), use
+%     % uppergram to judge
+%     if cb ~= 0 && cr ~= cb && cb == nb
+%         if strcmp(chordmode{2,ct},'maj/3') && cb == nb
+%             ns1 = tname2tnum('maj/3', chordmode);
+%             ns2 = tname2tnum('min', chordmode);
+%             s1 = sum(sumug(pitchTranspose(cb,ns1))); % maj/3 upper
+%             s2 = sum(sumug(pitchTranspose(cb,ns2))); % min upper
+%             if s2 > s1
+%                 rootgram(j) = cb;
+%                 treblegram(j) = tname2tnum('min', chordmode);
+%             end
+%         end
+%         if strcmp(chordmode{2,ct},'maj/5') && cb == nb
+%             ns1 = tname2tnum('maj/5', chordmode);
+%             ns2 = tname2tnum('maj', chordmode);
+%             s1 = sum(sumug(pitchTranspose(cb,ns1))); % maj/5 upper
+%             s2 = sum(sumug(pitchTranspose(cb,ns2))); % maj upper
+%             if s2 > s1
+%                 rootgram(j) = cb;
+%                 treblegram(j) = tname2tnum('maj', chordmode);
+%             end
+%         end
+%     end
     
 %     % **************************case 2***********************************
 %     
@@ -95,7 +213,8 @@ for j = 1:nslices
 %         end
 %     end
 %     
-%     % subcase2 -> cr == cb && cb == nb, such as (Bmaj <- Emaj/5, G#min <- Emaj/3), use
+%     % subcase2 -> cr == cb && cb == nb, such as (Bmaj <- Emaj/5, G#min <-
+%     Emaj/3), use 
 %     % uppergram to judge
 %     if cb ~= 0 && cr == cb && cb == nb
 %         if strcmp(chordmode{2,ct},'min')
