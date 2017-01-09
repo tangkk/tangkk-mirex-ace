0eff540e4dbca8d4f0baababc60525d0fdfff2fc
Junqi Deng <junqideng@Junqis-MacBook-Pro.local>
2017-01-09 10:25:02 +0800

    modify predict.py (loadpath, savepath)

--- nnn/predict.py
+++ nnn/predict.py
@@ -4,9 +4,10 @@ from sklearn import preprocessing
 import sys
 
 input = sys.argv[1]
-savepath = sys.argv[2]
+loadpath = sys.argv[2]
 nntype = sys.argv[3]
 invtype = sys.argv[4]
+savepath = sys.argv[5]
 
 # 1. load input (in .mat format)
 # standardize the input (scaling = 1)
@@ -16,7 +17,7 @@ X = preprocessing.scale(X, axis=1)
 # X = theano.shared(numpy.asarray(X, dtype=theano.config.floatX), borrow=True)
 
 # 2. load savez (in .pkl format or .npz format)
-savez = numpy.load(savepath)
+savez = numpy.load(loadpath)
             
 # 3. predict output using the model and the input
 # 4. save the prediction back to .mat
@@ -40,12 +41,12 @@ if nntype == 'svm' or nntype == 'knn':
     y_preds = predprobs(savez,X)
     y_preds[y_preds==0] = nclass
     print y_preds
-    sio.savemat('./data/temp/y_preds.mat', {'y_preds':y_preds})
+    sio.savemat(savepath+'y_preds.mat', {'y_preds':y_preds})
 else:
     y_probs, y_preds = predprobs(savez,X)
     # conform to matlab indexing standard
     y_probs = numpy.roll(y_probs,-1,axis=1)
     y_preds[y_preds==0] = nclass
     print y_preds
-    sio.savemat('./data/temp/y_probs.mat', {'y_probs':y_probs})
-    sio.savemat('./data/temp/y_preds.mat', {'y_preds':y_preds})
+    sio.savemat(savepath+'y_probs.mat', {'y_probs':y_probs})
+    sio.savemat(savepath+'y_preds.mat', {'y_preds':y_preds})
